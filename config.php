e83b64a1d3101f1131d3cd4a981f494c3c9b9b59
maregp <marek.prywer@gmail.com>
2013-10-20 13:02:35 +0200

    First commit

---
+++ config.php
@@ -0,0 +1,9 @@
+﻿<?php
+	
+	$mysql_host='localhost';
+	$port='3306';
+	$username='root';
+	$password='pass';
+	$database='toplist';
+	
+?>
---
+++ index.php
@@ -0,0 +1,22 @@
+﻿<?php
+	
+	include 'config.php';
+	
+	try
+		{
+			$pdo = new PDO('mysql:host='.$mysql_host.';dbname='.$database.';port='.$port, $username, $password );
+			echo 'Połączenie nawiązane!';
+		}
+	catch(PDOException $e)
+		{
+			echo 'Połączenie nie mogło zostać utworzone.<br />';
+		}
+
+	
+?>
+
+<!DOCTYPE html>
+<head>
+<title><?php echo $title; ?></title>
+test
+</head>
